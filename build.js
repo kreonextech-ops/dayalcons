db702a5da6385ce39a722003c70534e2af11a3a6
Kreonex Technologies <kreonextech@gmail.com>
2026-09-03 12:53:56 +0530

    fix: set CI=false for CRM build to prevent Vercel from failing on Babel warnings

--- build.js
+++ build.js
@@ -3,7 +3,11 @@ const { execSync } = require('child_process');
 
 console.log('--- Building CRM ---');
 execSync('npm install --legacy-peer-deps', { cwd: './crm', stdio: 'inherit' });
-execSync('npm run build', { cwd: './crm', stdio: 'inherit' });
+execSync('npm run build', { 
+  cwd: './crm', 
+  stdio: 'inherit',
+  env: { ...process.env, CI: 'false' } 
+});
 
 console.log('--- Copying CRM to public/crm ---');
 fs.rmSync('./public/crm', { recursive: true, force: true });
