3ac3fd000c50d990b5bb8bb8a41be8949ed5e7eb
Kreonex Technologies <kreonextech@gmail.com>
2026-09-07 14:54:52 +0530

    fix(crm): move hamburger menu to top left of navbar

--- crm/src/components/navbar/index.jsx
+++ crm/src/components/navbar/index.jsx
@@ -18,43 +18,46 @@ const Navbar = (props) => {
 
   return (
     <nav className="sticky top-4 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/10 p-2 backdrop-blur-xl dark:bg-[#0b14374d]">
-      <div className="ml-[6px]">
-        <div className="h-6 w-[224px] pt-1">
-          <a
-            className="text-sm font-normal text-navy-700 hover:underline dark:text-white dark:hover:text-white"
-            href=" "
-          >
-            Pages
-            <span className="mx-1 text-sm text-navy-700 hover:text-navy-700 dark:text-white">
-              {" "}
-              /{" "}
-            </span>
-          </a>
-          <Link
-            className="text-sm font-normal capitalize text-navy-700 hover:underline dark:text-white dark:hover:text-white"
-            to="#"
-          >
-            {brandText}
-          </Link>
-        </div>
-        <p className="shrink text-[33px] capitalize text-navy-700 dark:text-white">
-          <Link
-            to="#"
-            className="font-bold capitalize hover:text-navy-700 dark:hover:text-white"
-          >
-            {brandText}
-          </Link>
-        </p>
-      </div>
-
-      <div className="relative mt-[3px] flex h-[61px] w-full max-w-full sm:max-w-[355px] flex-grow items-center justify-end gap-6 rounded-full bg-white px-4 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-auto md:flex-grow-0 md:gap-4 xl:w-auto xl:gap-4">
-        {/* Dummy search removed */}
+      <div className="ml-[6px] flex items-center gap-4">
         <span
           className="flex cursor-pointer text-xl text-gray-600 dark:text-white xl:hidden"
           onClick={onOpenSidenav}
         >
-          <FiAlignJustify className="h-5 w-5" />
+          <FiAlignJustify className="h-6 w-6" />
         </span>
+        
+        <div>
+          <div className="h-6 w-[224px] pt-1">
+            <a
+              className="text-sm font-normal text-navy-700 hover:underline dark:text-white dark:hover:text-white"
+              href=" "
+            >
+              Pages
+              <span className="mx-1 text-sm text-navy-700 hover:text-navy-700 dark:text-white">
+                {" "}
+                /{" "}
+              </span>
+            </a>
+            <Link
+              className="text-sm font-normal capitalize text-navy-700 hover:underline dark:text-white dark:hover:text-white"
+              to="#"
+            >
+              {brandText}
+            </Link>
+          </div>
+          <p className="shrink text-[33px] capitalize text-navy-700 dark:text-white mt-2">
+            <Link
+              to="#"
+              className="font-bold capitalize hover:text-navy-700 dark:hover:text-white"
+            >
+              {brandText}
+            </Link>
+          </p>
+        </div>
+      </div>
+
+      <div className="relative mt-[3px] flex h-[61px] w-auto flex-grow items-center justify-end gap-4 rounded-full bg-white px-4 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-auto md:flex-grow-0 md:gap-4 xl:w-auto xl:gap-4">
+        {/* Dummy search removed */}
         {/* start Notification */}
         <Dropdown
           button={
