59ad383adbc83591074228f2661dde49e7513dd2
Kreonex Technologies <kreonextech@gmail.com>
2026-09-07 14:47:01 +0530

    fix(crm): responsive navbar and remove dummy search

--- crm/src/components/navbar/index.jsx
+++ crm/src/components/navbar/index.jsx
@@ -47,17 +47,8 @@ const Navbar = (props) => {
         </p>
       </div>
 
-      <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2">
-        <div className="flex h-full items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
-          <p className="pl-3 pr-2 text-xl">
-            <FiSearch className="h-4 w-4 text-gray-400 dark:text-white" />
-          </p>
-          <input
-            type="text"
-            placeholder="Search..."
-            class="block h-full w-full rounded-full bg-lightPrimary text-sm font-medium text-navy-700 outline-none placeholder:!text-gray-400 dark:bg-navy-900 dark:text-white dark:placeholder:!text-white sm:w-fit"
-          />
-        </div>
+      <div className="relative mt-[3px] flex h-[61px] w-full max-w-full sm:max-w-[355px] flex-grow items-center justify-end gap-6 rounded-full bg-white px-4 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-auto md:flex-grow-0 md:gap-4 xl:w-auto xl:gap-4">
+        {/* Dummy search removed */}
         <span
           className="flex cursor-pointer text-xl text-gray-600 dark:text-white xl:hidden"
           onClick={onOpenSidenav}
@@ -188,3 +179,4 @@ const Navbar = (props) => {
 };
 
 export default Navbar;
+
