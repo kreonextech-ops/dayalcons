d2311bee4a06e62f2982754f53f5fc453f95d11b
Kreonex Technologies <kreonextech@gmail.com>
2026-09-08 17:42:10 +0530

    fix(navbar): fix ESLint import order error

--- crm/src/components/navbar/index.jsx
+++ crm/src/components/navbar/index.jsx
@@ -1,8 +1,5 @@
 import { logAction } from "utils/auditLogger";
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "https://gdzligxryodasaxnhdco.supabase.co";
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkemxpZ3hyeW9kYXNheG5oZGNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNTg1MDUsImV4cCI6MjEwMjczNDUwNX0.AYTyAMf22g8au51ATReRQdQc2IzDLYQ2vtQH_Uyfrpg";
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 import React from "react";
 import Dropdown from "components/dropdown";
@@ -17,6 +14,10 @@ import {
 } from "react-icons/io";
 import NavbarAvatar from "./NavbarAvatar";
 
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "https://gdzligxryodasaxnhdco.supabase.co";
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkemxpZ3hyeW9kYXNheG5oZGNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNTg1MDUsImV4cCI6MjEwMjczNDUwNX0.AYTyAMf22g8au51ATReRQdQc2IzDLYQ2vtQH_Uyfrpg";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const Navbar = (props) => {
   const { onOpenSidenav, brandText } = props;
